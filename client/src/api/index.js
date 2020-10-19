8f6f3940c65a2e06942061560809fe6aa6ca534a
Lior Mangisto <liormangisto123@gmail.com>
2020-10-19 17:42:19 +0300

    fix api host name for web production

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,17 +1,29 @@
 // import axios from "axios";
 const axios = require("axios");
 
+function generateFullUrl() {
+  const domain =
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_DOMAIN
+      : "localhost:3001";
+  const http = process.env.NODE_ENV === "production" ? "https" : "http";
+  return `${http}://${domain}`;
+}
+
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: generateFullUrl() + "/api",
 });
 export const getAllGuides = () => api.get("/guides");
-export const searchGuideById = (id) =>
+export const searchGuideById = (id) => api.get(`guides/:${id}`);
+export const searchGuides = (country, lang, city, cost, comments) =>
   api.get(
-    `guides/:${id}`
-  );
-export const searchGuides = (country, lang, city, cost,comments) =>
-  api.get(
-    `search?${new URLSearchParams({ country, lang, city, cost,comments }).toString()}`
+    `search?${new URLSearchParams({
+      country,
+      lang,
+      city,
+      cost,
+      comments,
+    }).toString()}`
   );
 
 const apis = { getAllGuides, searchGuides };
